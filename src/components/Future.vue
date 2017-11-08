94fae4b261af9766cf05da6d6f405e0b89506d0f
Andy <andygnewman@gmail.com>
2017-11-08 18:37:03 +0000

    allow for 0 in getMonth

--- src/components/Future.vue
+++ src/components/Future.vue
@@ -1,7 +1,7 @@
 <template>
   <tbody>
     <tr v-for="(forecastDate, index) in forecastDates">
-      <th>{{`${new Date(forecastDate).getDate()}/${new Date(forecastDate).getMonth()}`}}</th>
+      <th>{{`${new Date(forecastDate).getDate()}/${new Date(forecastDate).getMonth() + 1}`}}</th>
       <td v-for="observation in observations">
         {{(Math.round(observation.forecasts[index] * 10 ) / 10).toFixed(1)}}
       </td>
